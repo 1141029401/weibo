2d37a128fd4c68c00106060067a300edd2e5034e
gongmeng <18588264128@163.com>
2019-10-21 09:20:01 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,6 +21,10 @@ class UsersController extends Controller
 
     }
 
+    public function index(){
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
 
     //注册用户方法
     public function create()
