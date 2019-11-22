25bae148115866cb67abfa23947f87816b737fae
gongmeng <18588264128@163.com>
2019-11-22 03:18:17 +0000

    关注按钮

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -19,12 +19,19 @@ class UserPolicy
         //
     }
 
+    //自己不能关注自己
+    public function follow(User $currentUser, User $user)
+    {
+        return $currentUser->id !== $user->id;
+    }
 
+    //自己才能更改自己
     public function update(User $currentUser, User $user)
     {
         return $currentUser->id === $user->id;
     }
 
+    //删除策略
     public function destroy(User $currentUser, User $user)
     {
         return $currentUser->is_admin && $currentUser->id !== $user->id;
