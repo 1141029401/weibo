f28e79fecea5909623130ad2d54e3a7f3bf2e848
gongmeng <18588264128@163.com>
2019-11-22 02:52:31 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 //创建/删除微博
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+//关注人列表，粉丝列表
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
